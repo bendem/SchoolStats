313071fe654cdeb6fc2c8e733de9e91a7cfaa12d
Curlybear <Tehcurlybear@gmail.com>
2015-02-13 14:23:26 +0100

    Getting ready to Stat2D

--- header/statistics/StatisticalSerie2D.hpp
+++ header/statistics/StatisticalSerie2D.hpp
@@ -2,7 +2,30 @@
 #define STATSTICALSERIE2D_HPP
 
 class StatisticalSerie2D {
+private:
+    Sample* sample;
+    const DataSource& dataSource;
+    float averageValue1;
+    float averageValue2;
+    float coefficientA;
+    float coefficientB;
+public:
+    StatisticalSerie2D(Sample*);
 
+    void display();
+    void forecast();
+    int menu();
+    void computeAverages();
+    void computeCoefficient();
+
+    float getCoefficientA() const;
+    void setCoefficientA(float coefficientA);
+    float getCoefficientB() const;
+    void setCoefficientB(float coefficientB);
+    float getAverageValue1() const;
+    void setAverageValue1(float averageValue1);
+    float getAverageValue2() const;
+    void setAverageValue2(float averageValue2);
 };
 
 #endif
