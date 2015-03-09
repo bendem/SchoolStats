ace6be4f9683a3fc716fab4e52a1a14cb9740e32
bendem <online@bendem.be>
2015-03-09 09:44:01 +0100

    Add min and max X and Y to data source 2D

--- header/data/DataSource2D.hpp
+++ header/data/DataSource2D.hpp
@@ -1,6 +1,7 @@
 #ifndef DATASOURCE2D_HPP
 #define DATASOURCE2D_HPP
 
+#include "data/Data2DIterator.hpp"
 #include "data/DataSource.hpp"
 
 class DataSource2D : public DataSource {
@@ -8,15 +9,41 @@ class DataSource2D : public DataSource {
 private:
     string subject2;
     DataSourceType type2;
+    float minX;
+    float minY;
+    float maxX;
+    float maxY;
 
 public:
     DataSource2D(string, string, string, DataSourceType, DataSourceType, const List<Data*>&);
     string getSubject2() const;
+
+    float getMinX() const;
+    float getMinY() const;
+    float getMaxX() const;
+    float getMaxY() const;
 
 };
 
 inline string DataSource2D::getSubject2() const {
     return this->subject2;
 }
+
+inline float DataSource2D::getMinX() const {
+    return this->minX;
+}
+
+inline float DataSource2D::getMinY() const {
+    return this->minY;
+}
+
+inline float DataSource2D::getMaxX() const {
+    return this->maxX;
+}
+
+inline float DataSource2D::getMaxY() const {
+    return this->maxY;
+}
+
 
 #endif
--- src/data/DataSource2D.cpp
+++ src/data/DataSource2D.cpp
@@ -6,4 +6,15 @@ DataSource2D::DataSource2D(string name, string subject1, string subject2,
     this->subject2 = subject2;
     this->type2 = type2;
 
+    Data2DIterator it(data);
+    this->minX = this->maxX = it.getX();
+    this->minY = this->maxY = it.getY();
+    ++it;
+    while(!it.end()) {
+        this->minX = min(this->minX, it.getX());
+        this->minY = min(this->minY, it.getY());
+        this->maxX = max(this->maxX, it.getX());
+        this->maxY = max(this->maxY, it.getY());
+        ++it;
+    }
 }
